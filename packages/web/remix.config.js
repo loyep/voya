00467caf3a1359d6f6488255ef022a3e8021a43b
Zack <lorenx@163.com>
2022-09-29 12:34:39 +0800

    feat: update build

--- packages/web/remix.config.js
+++ packages/web/remix.config.js
@@ -3,5 +3,8 @@
  */
 module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
+  browserBuildDirectory: "public/build",
+  publicPath: "/build/",
+  serverBuildDirectory: "server/build",
   ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
 };
